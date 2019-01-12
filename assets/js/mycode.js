0135aa3d2f507fca507c2a3f6b047e8a37854cf8
omid <evil13771022@gmail.com>
2019-01-12 09:59:20 +0330

    change rep

---
+++ assets/js/mycode.js
@@ -0,0 +1,34 @@
+AOS.init();
+$(document).ready(function () {
+    $("#about-we").click(function () {
+
+        $("#about").animatescroll(500);
+    });
+    $("#serves-we").click(function () {
+
+        $(".serves").animatescroll(500);
+    });
+    $("#home-we").click(function () {
+
+        $("#section2").animatescroll(500);
+    });
+    $("#portfolio-we").click(function () {
+
+        $("#portfolio").animatescroll(500);
+    });
+    $("#exti-we").click(function () {
+
+        $("#section3").animatescroll(500);
+    });
+    $("#theme-we").click(function () {
+
+        $("#section4").animatescroll(500);
+    });
+    $("#orginal-we").click(function () {
+
+        $("#section5").animatescroll(500);
+    });
+});
+
+
+
